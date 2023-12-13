6240f34822991aafd9cad97c4df9c8fda3d1d342
shivam6862 <shivam6862mau@gmail.com>
2023-12-13 22:07:31 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Welcome to the world of DevOps with Jenkins!");
+console.log(
+  "This application, powered by app.js, is your guide to mastering DevOps practices."
+);
+console.log("Get ready to automate, build, and deploy with ease.");
+console.log("Stay tuned for hands-on tutorials and practical insights.");
+console.log("Happy learning!");
